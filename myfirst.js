00b7ef1bc211b3eefb11860c786ca2bf53ae833b
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-02 14:53:16 +0200

    Primer módulo con fecha

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,9 @@
-var http = require('http');
+const http = require('http');
+const dt = require('./myfirstmodule');
 const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(PORT);
