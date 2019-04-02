ee384d7192f1f3115e1c06026389f9717153a307
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-02 15:03:45 +0200

    Voy a cambiar la organización de esto, por que qué calvario

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const url = require('url');
 const dt = require('./myfirstmodule');
 const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(PORT);
